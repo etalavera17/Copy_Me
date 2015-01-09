e77a55d0cf17c3e1417250cef3fe7476cdc77c8f
etalavera17 <etalavera17@punahou.edu>
2015-01-08 14:03:34 -1000

    Beta Testing

--- Copy_Me.ino
+++ Copy_Me.ino
@@ -53,10 +53,9 @@
 
 #include <MeggyJrSimple.h> // Required code, line 1 of 2.
 
-byte gameSequence [4], playerSequence [4];
+byte sequence [256], playerSequence [256], i, j, k, nextLight, nextNum, score;
 boolean correct, cont, repeat, start;
 
-int level = 1;
 
 
 void setup()                    // run once, when the sketch starts
@@ -69,8 +68,219 @@ void setup()                    // run once, when the sketch starts
 
 void loop()
 {
-  While
-}  
+  while (start)
+  {
+    if (!repeat)
+     {
+       randomSeed(millis());
+       StartScreen();
+       DisplaySlate();
+       delay(1000);
+       ClearSlate();
+       DisplaySlate();
+       delay(1000);
+       CheckButtonsDown();
+     }
+       
+      if (Button_A || Button_B)  //start game
+   
+   {    
+     
+     i=0;
+     
+     while (i<255)  //creates pseudo random color sequence
+     {
+      nextNum = random(1,5);
+      sequence[i]=nextNum;
+      i++;
+     }
+  
+      correct = true;
+      cont = true;
+      StartScreen();
+      DisplaySlate();
+      delay(1500);
+      SetAuxLEDs(score + 1);
+      start = false;
+  }
+  
+ }
+ 
+ if (cont && correct)  //plays back sequence until j reaches the current round
+{  
+  
+  j=0;
+  
+  while (j < score + 1)
+  {
+    
+    delay(200);
+    nextLight = sequence[j];
+    
+    switch (nextLight)
+      {
+        
+        case 1:
+        {
+        LightLeft();
+        delay(200);
+        StartScreen();
+        j++;
+        break;
+        }
+        
+        case 2:
+        {
+        LightRight();
+        delay(200);
+        StartScreen();
+        j++;
+        break;
+        }
+        
+        case 3:
+        {
+        LightUp();
+        delay(200);
+        StartScreen();
+        j++;
+        break;
+        }
+        
+        case 4:
+        {
+        LightDown();
+        delay(200);
+        StartScreen();
+        j++;
+        break;
+        }
+        
+      }
+      
+    Tone_Update();
+    cont = false;  //sentinel   
+ }
+ 
+
+if(!cont && correct);  //Player uses arrow keys to enter sequence.  If incorrect at any point game ends immediately.
+{
+  k=0;
+ 
+  while (k < score + 1 && correct )
+  {
+  
+    CheckButtonsDown();
+  
+    if (Button_Left)
+    {
+      
+     LightLeft();
+     StartScreen();
+     playerSequence[k] = 1;
+     
+     if (playerSequence[k] == sequence[k])
+     {
+      correct = true;
+      k++;
+     }
+     
+     else
+     {
+      correct = false;
+      end();
+     }
+     
+    }
+     
+    if (Button_Right)
+    {
+      
+     LightRight();
+     StartScreen();
+     playerSequence[k] = 2;
+     
+     if (playerSequence[k] == sequence[k])
+     {
+      correct = true;
+      k++;
+     }
+     
+      else
+     {
+      correct = false;
+      end();
+    }
+    
+  }
+  
+    if (Button_Up)
+    {
+    LightUp();
+     StartScreen();
+     playerSequence[k] = 3;
+     
+    if (playerSequence[k] == sequence[k])
+    {
+    correct = true;
+    k++;
+    }
+    
+    else
+    {
+     correct = false;
+     end();
+    }
+    
+   }
+  
+  if (Button_Down)
+  {
+   LightDown();
+   StartScreen();
+   playerSequence[k] = 4;
+   
+   if (playerSequence[k] == sequence[k])
+    {
+     correct = true;
+     k++;
+    }
+    
+    else
+    {
+      correct = false;
+      end();
+    }
+    
+  }
+  
+ }
+  
+ if (correct)
+ {
+   cont = true; //sentinel
+   score++;
+   SetAuxLEDs(score + 1);
+   delay(500);
+ }
+   
+ }
+ 
+  
+    
+}
+  
+DisplaySlate();
+Tone_Update();
+
+}
+
+
+
+
+   
+
+ 
 
 void end()
 {
@@ -81,34 +291,188 @@ void end()
  delay(1000);
  Tone_Update();
 
+  while(!correct && !start)
+    {
+     Up();
+     SetAuxLEDs(0);
+     delay(100);
+     ClearSlate();
+     redX();
+     Left();
+     SetAuxLEDs(score + 1);
+     delay(100);
+     ClearSlate();
+     redX();
+     Down();
+     SetAuxLEDs(0);
+     delay(100);
+     ClearSlate();
+     redX();
+     Right();
+     SetAuxLEDs(score + 1);
+     delay(100);
+     ClearSlate();
+     redX();
+     CheckButtonsPress();
+     
+        if (Button_A || Button_B) //reset game
+        {
+          ClearSlate();
+          DisplaySlate();
+          repeat = true;
+          score = 0;
+          start = true; 
+        }
+        
+     }   
+     
+}
+  
  
 
   
-}
+
 
 void Up()                // Draw Up arrow.
 {
-  DrawPx(4,7,Blue);
+  DrawPx(3,6,Red);  
+  DrawPx(3,5,Red);
+  DrawPx(4,6,Red);
+  DrawPx(4,5,Red);
+  DrawPx(2,5,Red);
+  DrawPx(3,7,Red);
+  DrawPx(4,7,Red);
+  DrawPx(5,5,Red);
+  DrawPx(2,6,Red);
+  DrawPx(5,6,Red);
   DisplaySlate();
 }
 
 void Down()
 {
-  DrawPx(4,0,Blue);
+  DrawPx(3,1,Violet);  
+  DrawPx(3,2,Violet);
+  DrawPx(4,1,Violet);
+  DrawPx(4,2,Violet);
+  DrawPx(2,2,Violet);
+  DrawPx(3,0,Violet);
+  DrawPx(4,0,Violet);
+  DrawPx(5,2,Violet);
+  DrawPx(2,1,Violet);
+  DrawPx(5,1,Violet);
   DisplaySlate();
 }
 
 void Left()
 {
-  DrawPx(0,4,Blue);
+  DrawPx(0,4,Green);
+  DrawPx(0,3,Green);
+  DrawPx(2,2,Green);
+  DrawPx(1,2,Green);
+  DrawPx(1,5,Green);
+  DrawPx(2,5,Green);
+  DrawPx(1,4,Green);  
+  DrawPx(1,3,Green);
+  DrawPx(2,4,Green);
+  DrawPx(2,3,Green);
   DisplaySlate();
 }
 
 void Right()
 {
-  DrawPx(7,4,Blue);
+  DrawPx(6,4,Yellow);  
+  DrawPx(6,3,Yellow);
+  DrawPx(5,4,Yellow);
+  DrawPx(5,3,Yellow);
+  DrawPx(5,2,Yellow);
+  DrawPx(7,3,Yellow);
+  DrawPx(7,4,Yellow);
+  DrawPx(5,5,Yellow);
+  DrawPx(6,2,Yellow);
+  DrawPx(6,5,Yellow);
   DisplaySlate();
 }
+
+void LightUp()                // Draw Up arrow.
+{
+  DrawPx(3,6,Red);  
+  DrawPx(3,5,Red);
+  DrawPx(4,6,Red);
+  DrawPx(4,5,Red);
+  DrawPx(2,5,Red);
+  DrawPx(3,7,Red);
+  DrawPx(4,7,Red);
+  DrawPx(5,5,Red);
+  DrawPx(2,6,Red);
+  DrawPx(5,6,Red);
+  DisplaySlate();
+  
+   Tone_Start(ToneD5, 200);
+  DisplaySlate();
+  delay(205);
+  Tone_Update();
+}
+
+void LightDown()
+{
+  DrawPx(3,1,Violet);  
+  DrawPx(3,2,Violet);
+  DrawPx(4,1,Violet);
+  DrawPx(4,2,Violet);
+  DrawPx(2,2,Violet);
+  DrawPx(3,0,Violet);
+  DrawPx(4,0,Violet);
+  DrawPx(5,2,Violet);
+  DrawPx(2,1,Violet);
+  DrawPx(5,1,Violet);
+  DisplaySlate();
+  
+   Tone_Start(ToneD5, 200);
+  DisplaySlate();
+  delay(205);
+  Tone_Update();
+}
+
+void LightLeft()
+{
+  DrawPx(0,4,Green);
+  DrawPx(0,3,Green);
+  DrawPx(2,2,Green);
+  DrawPx(1,2,Green);
+  DrawPx(1,5,Green);
+  DrawPx(2,5,Green);
+  DrawPx(1,4,Green);  
+  DrawPx(1,3,Green);
+  DrawPx(2,4,Green);
+  DrawPx(2,3,Green);
+  DisplaySlate();
+  
+   Tone_Start(ToneD5, 200);
+  DisplaySlate();
+  delay(205);
+  Tone_Update();
+}
+
+void LightRight()
+{
+  DrawPx(6,4,Yellow);  
+  DrawPx(6,3,Yellow);
+  DrawPx(5,4,Yellow);
+  DrawPx(5,3,Yellow);
+  DrawPx(5,2,Yellow);
+  DrawPx(7,3,Yellow);
+  DrawPx(7,4,Yellow);
+  DrawPx(5,5,Yellow);
+  DrawPx(6,2,Yellow);
+  DrawPx(6,5,Yellow);
+  DisplaySlate();
+  
+   Tone_Start(ToneD5, 200);
+  DisplaySlate();
+  delay(205);
+  Tone_Update();
+}
+
 
 void redX()
 {
@@ -139,5 +503,56 @@ void redX()
   DrawPx(7,7,Red);
   DisplaySlate();  
 }
+
+void StartScreen()
+{
+  DrawPx(0,4,White);//left
+  DrawPx(0,3,White);
+  DrawPx(2,2,White);
+  DrawPx(1,2,White);
+  DrawPx(1,5,White);
+  DrawPx(2,5,White);
+  DrawPx(1,4,White);  
+  DrawPx(1,3,White);
+  DrawPx(2,4,White);
+  DrawPx(2,3,White);
+
+  
+  DrawPx(6,4,White);  //right
+  DrawPx(6,3,White);
+  DrawPx(5,4,White);
+  DrawPx(5,3,White);
+  DrawPx(5,2,White);
+  DrawPx(7,3,White);
+  DrawPx(7,4,White);
+  DrawPx(5,5,White);
+  DrawPx(6,2,White);
+  DrawPx(6,5,White);
+  
+  DrawPx(3,6,White);  //up
+  DrawPx(3,5,White);
+  DrawPx(4,6,White);
+  DrawPx(4,5,White);
+  DrawPx(2,5,White);
+  DrawPx(3,7,White);
+  DrawPx(4,7,White);
+  DrawPx(5,5,White);
+  DrawPx(2,6,White);
+  DrawPx(5,6,White);
+  
+  DrawPx(3,1,White);  //down
+  DrawPx(3,2,White);
+  DrawPx(4,1,White);
+  DrawPx(4,2,White);
+  DrawPx(2,2,White);
+  DrawPx(3,0,White);
+  DrawPx(4,0,White);
+  DrawPx(5,2,White);
+  DrawPx(2,1,White);
+  DrawPx(5,1,White);
+  
+  DisplaySlate();
+}
+
 
   
